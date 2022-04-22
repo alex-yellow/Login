0073c17bd7e84757a3f26fcba3e96727b6ef011d
alex-yellow <72997598+alex-yellow@users.noreply.github.com>
2022-04-22 19:25:10 +0300

    Add files via upload

---
+++ Login/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserContext db = new UserContext();
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                ModelState.Clear();
+                ViewBag.Message = user.Name + " " + "успешно зарегестрирован";
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var usr = db.Users.Single(u => u.Name == user.Name && u.Password == user.Password);
+            if(usr==null)
+            {
+                return RedirectToAction("Index");
+            }
+            Session["Id"] = usr.Id.ToString();
+            Session["Name"] = usr.Name.ToString();
+            return RedirectToAction("LoggedIn");
+        }
+        public ActionResult LoggedIn()
+        {
+            if(Session["Id"]!=null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ Login/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Login.Models
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        [Display(Name ="Логин")]
+        [Required(ErrorMessage ="Поле Логин должно быть заполнено")]
+        public string Name { get; set; }
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Поле Email должно быть заполнено")]
+        public string Email { get; set; }
+        [Display(Name = "Пароль")]
+        [Required(ErrorMessage = "Поле Пароль должно быть заполнено")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("Password", ErrorMessage ="Пароли должны совпадать")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
